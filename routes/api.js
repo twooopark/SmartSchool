83c77abcbe9b24a9e7995225c30eab10f070847b
twooopark <twooo.park@gmail.com>
2017-11-15 10:10:40 +0900

    DashBoard Sensor API

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,24 @@
 var router = express.Router()
+
+router.get("/dashboard-data", (req, res) => {
+  var query =
+  'SELECT INFO AS TYPE, round(avg(DATA)) '+
+  'FROM sensor_data_update '+
+  'join sensor on sensor.TYPE = sensor_data_update.TYPE '+
+  'group by TYPE '+
+  'ORDER BY TYPE; ';
+
+  db.query(query, (err, result) => {
+    if(err) {
+      console.error(query, err)
+      res.end("error")
+      return
+    }
+    res.end(JSON.stringify(result))
+  })
+})
+
 router.get("/error-data", (req, res) => {
-  var data = req.query || {}
-  var start_day = data.start_day || data["start-day"] || "20171020"
-  var end_day = data.end_day || data["end-day"] || "20171120"
   var query =
   'SELECT loc, time, type, cnt ' +
   'from sensor_error_hourly order by time';
@@ -16,10 +32,8 @@ router.get("/error-data", (req, res) => {
     res.end(JSON.stringify(result))
   })
 })
+
 router.get("/sensor_daily-data", (req, res) => {
-  var data = req.query || {}
-  var start_day = data.start_day || data["start-day"] || "20171020"
-  var end_day = data.end_day || data["end-day"] || "20171120"
   var query =
   'SELECT loc, time, type, avge ' +
   'from sensor_data_daily order by time';
@@ -33,6 +47,7 @@ router.get("/sensor_daily-data", (req, res) => {
     res.end(JSON.stringify(result))
   })
 })
+
 router.get("/sensor-data", (req, res) => {
   var query =
   'SELECT LOCATION AS LOC, MAC_HEX AS MAC, INFO AS TYPE, DATA, DATE_FORMAT(TIME, "%Y-%m-%d %H:%i:%s") AS TIME '+
@@ -66,15 +81,14 @@ router.get("/class-data", (req, res) => {
   })
 })
 
-//http://www.opentechguides.com/how-to/article/nodejs/124/express-mysql-json.html
 router.post("/class-data-update", (req, res) => {
   var jsondata = req.body;
   var values = "";
   for(var i=0; i< jsondata.length; i++){
     values += "("+jsondata[i].No+",\""+jsondata[i].Location+"\",\""+jsondata[i].Serial+"\","+jsondata[i].MAC_DEC+",\""+jsondata[i].MAC_HEX+"\","+jsondata[i].X+","+jsondata[i].Y+"),";
   }
-  values = values.slice(0,-1); //마지막 쉼표 제거
-  values += "ON DUPLICATE KEY UPDATE location=VALUES(location),COORD_X=VALUES(coord_x),coord_y=VALUES(coord_y);" //PK 제외
+  values = values.slice(0,-1);
+  values += "ON DUPLICATE KEY UPDATE location=VALUES(location),COORD_X=VALUES(coord_x),coord_y=VALUES(coord_y);"
   
   var query = "INSERT INTO classroom (NO,LOCATION,SERIAL,MAC_DEC,MAC_HEX,COORD_X,COORD_Y) VALUES " ;
   query += values;
