0330897340a389646950a6f852832dac9f751e44
twooopark <twooo.park@gmail.com>
2017-11-22 19:03:15 +0900

    교실관리, 학생정보 API, 페이지 제작

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ router.get("/dashboard-data", (req, res) => {
   'SELECT INFO AS type, round(avg(DATA)) as data '+
   'FROM sensor_data_update '+
   'join sensor on sensor.TYPE = sensor_data_update.TYPE '+
-  'group by TYPE '+
+  'GROUP BY TYPE '+
   'ORDER BY TYPE; ';
 
   db.query(query, (err, result) => {
@@ -21,7 +21,7 @@ router.get("/dashboard-data", (req, res) => {
 router.get("/error-data", (req, res) => {
   var query =
   'SELECT loc, time, type, cnt ' +
-  'from sensor_error_hourly order by time';
+  'FROM sensor_error_hourly ORDER BY time';
 
   db.query(query, (err, result) => {
     if(err) {
@@ -36,7 +36,7 @@ router.get("/error-data", (req, res) => {
 router.get("/sensor_daily-data", (req, res) => {
   var query =
   'SELECT type, time, avge, loc '+
-  'from sensor_data_daily order by type, time;';//hourly order by type, time;';//
+  'FROM sensor_data_daily ORDER BY type, time;';//hourly order by type, time;';//
 
   db.query(query, (err, result) => {
     if(err) {
@@ -48,15 +48,14 @@ router.get("/sensor_daily-data", (req, res) => {
   })
 })
 
-//hourly ? daily? where?
+// daily? hourly? where?
 router.post("/sensor_dh-data", (req, res) => {
   var jsondata = req.body;
   var query =
   'SELECT type, time, avge, loc '+
-  'from sensor_data_'+jsondata.sel;
+  'FROM sensor_data_'+jsondata.sel;
   if(jsondata.loc) query += ' where loc = \"'+jsondata.loc+'\" ';
-  query += ' order by type, time ';
-  console.log(query);
+  query += ' ORDER BY type, time ';
   db.query(query, (err, result) => {
     if(err) {
       console.error(query, err)
@@ -86,8 +85,10 @@ router.get("/sensor-data", (req, res) => {
 
 router.get("/class-data", (req, res) => {
   var query =
-  'SELECT NO, LOCATION as LOC, SERIAL, DATE_FORMAT(REPLACED_DATE, "%Y-%m-%d") AS TIME, MAC_HEX as MAC, DESCRIPTION, COORD_X as x, COORD_Y as y '+
-  'FROM classroom ORDER BY LOC'
+  'SELECT (@cnt := @cnt + 1) as NO, LOCATION as LOC, SERIAL, DATE_FORMAT(REPLACED_DATE, "%Y-%m-%d") AS TIME, MAC_HEX as MAC, DESCRIPTION, COORD_X as x, COORD_Y as y '+
+  'FROM classroom '+
+  'CROSS JOIN (SELECT @cnt := 0) AS dummy '+
+  'ORDER BY LOC';
 
   db.query(query, (err, result) => {
     if(err) {
@@ -99,17 +100,61 @@ router.get("/class-data", (req, res) => {
   })
 })
 
+router.get("/class_mng-data", (req, res) => {
+  var query =
+  'SELECT (@cnt := @cnt + 1) as no, c.LOCATION as class_name, t.NAME as teacher_name, s.cnt as student_cnt, \"유\" as sensor_in,c.SERIAL as device_id '+
+  'FROM classroom as c left outer join teacher as t on c.MAC_DEC = t.CLASSROOM_MAC, '+
+    '(SELECT student.CLASSROOM_MAC, count(*) as cnt '+
+    'FROM student '+
+    'CROSS JOIN (SELECT @cnt := 0) as dummy '+
+    'GROUP BY student.CLASSROOM_MAC) as s '+
+  'WHERE c.MAC_DEC = s.CLASSROOM_MAC ';
+
+  db.query(query, (err, result) => {
+    if(err) {
+      console.error(query, err)
+      res.end("error")
+      return
+    }
+    res.end(JSON.stringify(result))
+  })
+})
+
+router.post("/student-data", (req, res) => {
+  var jsondata = req.body;
+  var query =
+  'SELECT s.name as student_name, \"등교\" as student_state '+
+  'FROM classroom as c, student as s '+
+  'WHERE c.MAC_DEC = s.CLASSROOM_MAC ';
+  if(jsondata.c_name) 
+    query += 'and c.LOCATION = \"'+jsondata.c_name+'\" ';
+  
+  console.log(query)
+  db.query(query, (err, result) => {
+    if(err) {
+      console.error(query, err)
+      res.end("error")
+      return
+    }
+    res.end(JSON.stringify(result))
+  })
+})
+
+
 function HexToDec(Hex){
-  var tempArr = Hex.split("-");
+  var tempArr, temp, Dec;
+  tempArr = Hex.split("-");
   tempArr.reverse();
-  var temp = tempArr.toString();
+  temp = tempArr.toString();
   temp = temp.replace(/,/g,"");
-  var Dec = parseInt(temp, 16);
+  Dec = parseInt(temp, 16);
   return Dec;
 }
 
 router.post("/class-data-update", (req, res) => {
   var jsondata = req.body;
+  
+  var query = "DELETE FROM classroom; INSERT INTO classroom (NO,LOCATION,SERIAL,MAC_DEC,MAC_HEX,COORD_X,COORD_Y,REPLACED_DATE,DESCRIPTION) VALUES " ;
   var values = "";
   for(var i=0; i< jsondata.length; i++){
     values += "("+jsondata[i].번호+",\""+jsondata[i].위치+"\",\""+jsondata[i].이름+"\","+HexToDec(jsondata[i].물리주소)+","+
@@ -118,7 +163,6 @@ router.post("/class-data-update", (req, res) => {
   values = values.slice(0,-1);
   values += "ON DUPLICATE KEY UPDATE LOCATION=VALUES(LOCATION),COORD_X=VALUES(COORD_X),COORD_Y=VALUES(COORD_Y),REPLACED_DATE=VALUES(REPLACED_DATE),DESCRIPTION=VALUES(DESCRIPTION);"
   
-  var query = "DELETE FROM classroom; INSERT INTO classroom (NO,LOCATION,SERIAL,MAC_DEC,MAC_HEX,COORD_X,COORD_Y,REPLACED_DATE,DESCRIPTION) VALUES " ;
   query += values;
   db.query(query, (err, result) => {
     if(err) {
